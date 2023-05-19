5307f7583c066c89d440aa7515060669d2fe6328
andreagrassi11 <andreagrassi200a@gmail.com>
2023-05-19 09:41:20 +0200

    Modifiche

--- quickshare_api/models.py
+++ quickshare_api/models.py
@@ -5,3 +5,15 @@ class Image(models.Model):
     id = models.IntegerField(primary_key = True)
     image_name = models.CharField(max_length=180)
     upload_data = models.DateField()
+
+class List(models.Model):  
+    id = models.IntegerField(primary_key = True)
+    title = models.CharField(max_length=50)
+    descrizione = title = models.CharField(max_length=150)
+    create_data = models.DateField()
+
+class ListElement(models.Model):  
+    id = models.IntegerField(primary_key = True)
+    description = models.CharField(max_length=130)
+    do =  models.BooleanField()
+    fk_list = models.ForeignKey(List, on_delete=models.CASCADE)
--- quickshare_api/serializers.py
+++ quickshare_api/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Image
+from .models import Image, List, ListElement
 
 class QuickshareSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = ["id","image_name","upload_data"]
+
+    class Meta:
+        model = List
+        fields = ["id","title","description"]
+
+    class Meta:
+        model = ListElement
+        fields = ["id","title","description"]
